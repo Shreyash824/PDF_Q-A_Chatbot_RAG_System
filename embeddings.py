1c58baa2c33a3a17e77a6161d34a9bba0e1e8531
Shreyash824 <123435974+Shreyash824@users.noreply.github.com>
2026-08-27 19:10:54 +0530

    Update embeddings.py

--- embeddings.py
+++ embeddings.py
@@ -1,13 +1,3 @@
-#Split Into Chunks
-
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-
-splitter = RecursiveCharacterTextSplitter(
-    chunk_size=1000,
-    chunk_overlap=200
-)
-chunks = splitter.split_documents(documents)
-
 #Create Embeddings
 
 from langchain.embedding
@@ -15,7 +5,3 @@ import OpenAIEmbeddings
 embeddings = OpenAIEmbeddings()
 
 
-#Store in Vector Database
-
-from langchain.vectorstores 
-import Chromadb = Chroma.from_documents(chunks, embeddings)
