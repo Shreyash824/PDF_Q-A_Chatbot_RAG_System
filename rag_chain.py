e39767e751f0d53a48a79ab3d2629a3e76d5aee0
Shreyash824 <123435974+Shreyash824@users.noreply.github.com>
2026-08-27 19:04:36 +0530

    Remove chunk splitting from document processing
    Removed chunk splitting functionality from rag_chain.py.

--- rag_chain.py
+++ rag_chain.py
@@ -5,16 +5,6 @@ loader=
 PyPDFLoader("document.pdf")
 documents = loader.load()
 
-#Split Into Chunks
-
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-
-splitter = RecursiveCharacterTextSplitter(
-    chunk_size=1000,
-    chunk_overlap=200
-)
-chunks = splitter.split_documents(documents)
-
 #Search Relevant Chunks
 
 docs = db.similarity_search(question, k=3)
