96252f0b9d536f165b23fef6fdecda19f9b5a2b6
Shreyash824 <123435974+Shreyash824@users.noreply.github.com>
2026-08-31 18:18:05 +0530

    Delete documents.py

--- documents.py
+++
@@ -1,6 +0,0 @@
-from langchain_community.document_loaders import PyPDFLoader
-
-def load_documents(pdf_path):
-    """Load documents from the provided PDF path."""
-    loader = PyPDFLoader(pdf_path)
-    return loader.load()
